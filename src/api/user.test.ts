1583774f5d13b563aa8dfbeca1e2cd5cb3be4fa6
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2019-08-15 17:39:19 +0700

    Delete company testing + add CUD operation testings for user API

--- src/api/user.test.ts
+++ src/api/user.test.ts
@@ -3,6 +3,12 @@ import { api } from './config'
 
 const { uri: proxy }: { uri: string } = api
 
+interface IUser {
+  id?: string
+  name: string
+  age: number
+}
+
 describe('User\'s API asynchronus testing', () => {
 
   it('Should return users data', async () => {
@@ -32,6 +38,54 @@ describe('User\'s API asynchronus testing', () => {
       const { response: { data, status } } = err
       expect(status).toBe(404)
       expect(JSON.stringify(data)).toBe('{}')
+    }
+  })
+
+  it('Should add new user data', async () => {
+    try {
+      const new_user: IUser = {
+        name: 'Rico Sandyca Novenza',
+        age: 20
+      }
+      const { data } = await axios.post(`${proxy}/users`, new_user)
+      expect(data).toBeDefined()
+      expect(data instanceof Object).toBe(true)
+      expect(data.name === new_user.name && data.age === new_user.age).toBe(true)
+    } catch (err) {
+      const { response: { status } } = err
+      expect(status).toBe(200)
+    }
+  })
+
+  it('Should update user data', async () => {
+    try {
+      const { data: users } = await axios.get(`${proxy}/users`)
+      const target_user = users[users.length - 1]
+      const update_user: IUser = {
+        name: 'Rico Sandyca Updated',
+        age: 21
+      }
+      const { data } = await axios.put(`${proxy}/users/${target_user.id}`, update_user)
+      expect(data).toBeDefined()
+      expect(data instanceof Object).toBe(true)
+      expect(data.name === update_user.name && data.age === update_user.age).toBe(true)
+    } catch (err) {
+      const { response: { status } } = err
+      expect(status).toBe(200)
+    }
+  })
+
+  it('Should delete last user in lists', async () => {
+    try {
+      const { data: users } = await axios.get(`${proxy}/users`)
+      const target_user = users[users.length - 1]
+      const { data } = await axios.delete(`${proxy}/users/${target_user.id}`)
+      expect(data).toBeDefined()
+      expect(data instanceof Object).toBe(true)
+      expect(JSON.stringify(data)).toBe('{}')
+    } catch (err) {
+      const { response: { status } } = err
+      expect(status).toBe(200)
     }
   })
 
